fece19098a591e62fde0d2d975dcce079661c125
jacques-tsongo <kaserekatsongojacques2023@gmail.com>
2026-07-23 11:30:56 +0200

    route admin

---
+++ src/routes/admin.routes.js
@@ -0,0 +1,1 @@
+const router=require('express').Router(); const c=require('../controllers/admin.controller'); const {authenticate,authorize}=require('../middlewares/auth.middleware'); router.use(authenticate,authorize('administrateur')); router.get('/utilisateurs',c.users); router.patch('/utilisateurs/:id/statut',c.userStatus); router.get('/statistiques',c.stats); module.exports=router;
