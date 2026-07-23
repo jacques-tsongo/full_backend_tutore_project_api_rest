3fe0d194176931f81c9791f79aeb1cdbfced3469
jacques-tsongo <kaserekatsongojacques2023@gmail.com>
2026-07-23 11:31:44 +0200

    route authentification

---
+++ src/routes/auth.routes.js
@@ -0,0 +1,2 @@
+const router=require('express').Router(); const c=require('../controllers/auth.controller'); const v=require('../validators/auth.validator'); const valid=require('../middlewares/validate.middleware'); const {authenticate}=require('../middlewares/auth.middleware');
+router.post('/register',v.register,valid,c.register); router.post('/login',v.login,valid,c.login); router.get('/me',authenticate,c.me); router.put('/me',authenticate,c.updateMe); router.post('/logout',authenticate,c.logout); module.exports=router;
