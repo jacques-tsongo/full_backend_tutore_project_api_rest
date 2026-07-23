317c4d96c244e0a9b0c898c6f98f9667bf2ff2bb
jacques-tsongo <kaserekatsongojacques2023@gmail.com>
2026-07-23 11:42:02 +0200

    message route

---
+++ src/routes/message.routes.js
@@ -0,0 +1,1 @@
+const router=require('express').Router(); const c=require('../controllers/message.controller'); const {authenticate}=require('../middlewares/auth.middleware'); router.use(authenticate); router.post('/',c.send); router.get('/',c.conversations); router.get('/:userId',c.conversation); module.exports=router;
